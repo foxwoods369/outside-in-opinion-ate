89e16c0f7e848e1abe8b191f02fd9ad2525d7535
Skyler Dawson <skyler@skylerdawson.com>
2021-04-30 21:05:19 -0500

    Add RestaurantList display of store data

--- src/components/RestaurantList.vue
+++ src/components/RestaurantList.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>RestaurantList</div>
+  <ul>
+    <li
+      v-for="restaurant in restaurants"
+      :key="restaurant.id"
+      data-testid="restaurant"
+    >
+      {{ restaurant.name }}
+    </li>
+  </ul>
 </template>
 
 <script>
+import {mapActions, mapState} from 'vuex';
+
 export default {
   name: 'RestaurantList',
+  mounted() {
+    this.loadRestaurants();
+  },
+  methods: mapActions({
+    loadRestaurants: 'restaurants/load',
+  }),
+  computed: mapState({
+    restaurants: state => state.restaurants.records,
+  }),
 };
 </script>
---
+++ tests/unit/components/RestaurantList.spec.js
@@ -0,0 +1,62 @@
+import Vuex from 'vuex';
+import {mount, createLocalVue} from '@vue/test-utils';
+import RestaurantList from '@/components/RestaurantList';
+
+describe('RestaurantList', () => {
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+
+  it('loads a list of restaurants on mount', () => {
+    const restaurantsModule = {
+      namespaced: true,
+      actions: {
+        load: jest.fn().mockName('load'),
+      },
+    };
+
+    const store = new Vuex.Store({
+      modules: {
+        restaurants: restaurantsModule,
+      },
+    });
+
+    mount(RestaurantList, {localVue, store});
+
+    expect(restaurantsModule.actions.load).toHaveBeenCalled();
+  });
+
+  it('displays the list of restaurants', () => {
+    const records = [
+      {id: 1, name: 'Sushi Place'},
+      {id: 2, name: 'Pizza Place'},
+    ];
+
+    const restaurantsModule = {
+      namespaced: true,
+      state: {records},
+      actions: {
+        load: jest.fn().mockName('load'),
+      },
+    };
+
+    const store = new Vuex.Store({
+      modules: {
+        restaurants: restaurantsModule,
+      },
+    });
+
+    const wrapper = mount(RestaurantList, {localVue, store});
+
+    const firstRestaurantName = wrapper
+      .findAll('[data-testid="restaurant"]')
+      .at(0)
+      .text();
+    expect(firstRestaurantName).toBe('Sushi Place');
+
+    const secondRestaurantName = wrapper
+      .findAll('[data-testid="restaurant"]')
+      .at(1)
+      .text();
+    expect(secondRestaurantName).toBe('Pizza Place');
+  });
+});
