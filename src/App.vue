64bf8231a0d0f6491a351142d8b078ca72a989fc
Skyler Dawson <skyler@skylerdawson.com>
2021-05-31 21:36:37 -0500

    Updated styling using vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <div id="app">
-    <RestaurantIndexPage />
-  </div>
+  <v-app>
+    <v-app-bar color="primary" fixed app>
+      <v-toolbar-title>Foxwoods' Opinion Ate</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <RestaurantIndexPage />
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
