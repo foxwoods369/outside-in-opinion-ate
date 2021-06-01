be1da6188d355c5e0e1ecd9481680db411c7ce17
Skyler Dawson <skyler@skylerdawson.com>
2021-05-31 22:00:38 -0500

    Added loading notification

--- tests/unit/store/restaurants.spec.js
+++ tests/unit/store/restaurants.spec.js
@@ -6,26 +6,63 @@ describe('restaurants', () => {
   const localVue = createLocalVue();
   localVue.use(Vuex);
 
+  describe('initially', () => {
+    it('does not have the loading flag set', () => {
+      const store = new Vuex.Store({
+        modules: {
+          restaurants: restaurants(),
+        },
+      });
+
+      expect(store.state.restaurants.loading).toBe(false);
+    });
+  });
   describe('load action', () => {
-    it('stores the restaurants', async () => {
+    describe('while loading', () => {
+      it('sets a loading flag', () => {
+        const api = {
+          loadRestaurants: () => new Promise(() => {}),
+        };
+
+        const store = new Vuex.Store({
+          modules: {
+            restaurants: restaurants(api),
+          },
+        });
+
+        store.dispatch('restaurants/load');
+        expect(store.state.restaurants.loading).toBe(true);
+      });
+    });
+    describe('when loading succeeds', () => {
       const records = [
         {id: 1, name: 'Sushi Place'},
         {id: 2, name: 'Pizz Place'},
       ];
 
-      const api = {
-        loadRestaurants: () => Promise.resolve(records),
-      };
+      let store;
 
-      const store = new Vuex.Store({
-        modules: {
-          restaurants: restaurants(api),
-        },
+      beforeEach(() => {
+        const api = {
+          loadRestaurants: () => Promise.resolve(records),
+        };
+
+        store = new Vuex.Store({
+          modules: {
+            restaurants: restaurants(api),
+          },
+        });
+
+        return store.dispatch('restaurants/load');
       });
 
-      await store.dispatch('restaurants/load');
+      it('clears the loading flag', () => {
+        expect(store.state.restaurants.loading).toBe(false);
+      });
 
-      expect(store.state.restaurants.records).toEqual(records);
+      it('stores the restaurants', async () => {
+        expect(store.state.restaurants.records).toEqual(records);
+      });
     });
   });
 });
